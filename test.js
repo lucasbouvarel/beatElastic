8751dc06af67d78ca97fd282e315c9807f9729cb
Bouvarel Lucas <lbouvarel@TC405-112-14.insa-lyon.fr>
2019-11-12 11:43:40 +0100

    twitter api connection and simple request

--- test.js
+++ test.js
@@ -1,20 +1,75 @@
-var XMLHttp.XMLHttpRequest;
-
-var bearertoken="cLlDel9yLZdWKO1BXXjIDoQPm:GMbNW0O4bvZoQi4znMgNt5TV3W3BnoPg9vZmegakHS8Rkj52GT"
-var bearertoken64="Y0xsRGVsOXlMWmRXS08xQlhYaklEb1FQbTpHTWJOVzBPNGJ2Wm9RaTR6bk1nTnQ1VFYzVzNCbm9QZzl2Wm1lZ2FrSFM4UmtqNTJHVA=="
-
-const Http = new XMLHttpRequest();
-const url='https://api.twitter.com/oauth2/token';
-Http.open("POST", url);
-Http.setRequestHeader("Authorization","Basic <Y0xsRGVsOXlMWmRXS08xQlhYaklEb1FQbTpHTWJOVzBPNGJ2Wm9RaTR6bk1nTnQ1VFYzVzNCbm9QZzl2Wm1lZ2FrSFM4UmtqNTJHVA==>")
-Http.setRequestHeader("Content-Type","application/x-www-form-urlencoded;charset=UTF-8")
-params='oauth_verifier=GMbNW0O4bvZoQi4znMgNt5TV3W3BnoPg9vZmegakHS8Rkj52GT'
-Http.send("grant_type=client_credentials");
-
-Http.onreadystatechange = (e) => {
-  console.log("aaa")
-  console.log(Http.status)
-  if (this.readyState === XMLHttpRequest.DONE && Http.status === 200) {
-    console.log(Http.responseText
+var request = require('./node_modules/request-promise');
+var fs = require("fs")
+var authenticate = () => {
+  var options = {
+    method : "POST",
+    uri : 'https://api.twitter.com/oauth2/token',
+    headers : {
+      "Authorization" : "Basic <Y0xsRGVsOXlMWmRXS08xQlhYaklEb1FQbTpHTWJOVzBPNGJ2Wm9RaTR6bk1nTnQ1VFYzVzNCbm9QZzl2Wm1lZ2FrSFM4UmtqNTJHVA==>",
+      "Content-Type" : "application/x-www-form-urlencoded;charset=UTF-8",
+    },
+    json : true,
+    form : {
+      grant_type:"client_credentials"
+    }
   }
+  var token
+  return new Promise(function(resolve, reject){
+    request(options)
+    .then(body => {
+      token=body.access_token
+      resolve(token)
+    })
+    .catch(err => {
+      reject(err.message)
+    })
+  })
 }
+
+
+var makeRequest = (url, bearer, parameters) => {
+  var options = {
+    uri : url,
+    headers : {
+      "Authorization" : "Bearer "+ bearer,
+      "Accept-Encoding" : "json",
+      "User-Agent" : "BeatElastic",
+    }
+  }
+  return new Promise((resolve,reject) => {
+    request(options)
+    .then(body => {
+      resolve(body)
+    })
+    .catch(err => {
+      reject(err.message)
+    })
+  })
+}
+var writeResponse = (text, name_file) => {
+  fs.writeFile("/tmp/"+name_file, text, (err) =>{
+    if(err){
+      console.log("Something occured : ",err)
+    }
+    console.log("The file was saved")
+  })
+}
+
+
+authenticate()
+.then(token => {
+  console.log("I am authenticated : ", token)
+  url='https://api.twitter.com/1.1/search/tweets.json?q=from%3Atwitterdev&result_type=mixed&count=2'
+  makeRequest(url, token)
+  .then(body => {
+    var texts = JSON.parse(body).statuses.map(stat => stat.text)
+    console.log("Here is the response : \n", texts)
+    writeResponse(JSON.stringify(texts), "response.json")
+  })
+  .catch(err => {
+    console.log("Bad response : ", err)
+  })
+})
+.catch(err => {
+  console.log("I am not connected... ", err)
+})
